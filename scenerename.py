64d5995f52efc74c5d5ed69b9479dffd663742c7
Knio <theknio@gmail.com>
2009-05-25 20:44:25 -0600

    added a --help option

--- scenerename.py
+++ scenerename.py
@@ -143,7 +143,38 @@ def main():
     
     while args:
         a = args.pop(0)
-        if a == '--confirm' and allow_rename:
+        if a == '-h' or a == '--help' or a == '-help':
+            print '''
+Command Line Arguments
+
+-s / --samples          Test out the parsing on a variety of samples which 
+                        were designed to challenge the script.
+
+-d / --dict             Displays a verbose dictionary of all the matched 
+                        elements in the name.
+
+-o / --output           Specifies an alternate output format. Use the python 
+                        string format notation coupled with the names from -d.
+            (i.e. -o "%(show)s %(season)sx%(episode)2s - %(title)s.%(ext)s")
+
+-n / --none             Alternate string to use for unknown titles. 
+                        (Default: "None")
+
+-f1 / --filter-before   Apply pre-parsing rules to difficult names to aid in
+                        proper parsing. (i.e. -f1 '[{group|name]}'='groupname')
+
+-f2 / --filter-after    Apply post-parsing replacements to difficult names to 
+                        aid in proper parsing. Works exactly like -f1 except 
+                        the replacement is done on the new filename.
+
+--confirm               Be default the script only display how it would rename 
+                        the files but will not perform the action. 
+                        Append this to perform the renaming.
+'''
+            return
+            
+            
+        elif a == '--confirm' and allow_rename:
             confirm = True
             
         elif a == '-s' or a == '--samples':
